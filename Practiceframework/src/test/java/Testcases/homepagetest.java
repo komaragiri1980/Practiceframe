f4eef0ffb7a9a656b114f0d5fc029876bf1e6f12
komaragiri1980 <137947770+komaragiri1980@users.noreply.github.com>
2023-07-13 20:10:21 -0500

    added report

--- Practiceframework/src/test/java/Testcases/homepagetest.java
+++ Practiceframework/src/test/java/Testcases/homepagetest.java
@@ -1,5 +1,7 @@
 package Testcases;
 import org.testng.Assert;
+import org.testng.ITestResult;
+
 import java.io.IOException;
 
 import org.testng.annotations.AfterMethod;
@@ -10,6 +12,7 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -17,6 +20,7 @@ import base.Basetest;
 import pages.Homepage;
 import pages.Loginpage;
 import pages.Registerpage;
+import utilities.Testutil;
 
 public class homepagetest extends Basetest {
 	
@@ -42,7 +46,7 @@ public class homepagetest extends Basetest {
    @BeforeTest
 	 public void setExtent() {
 	  // specify location of the report
-	  htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/test-output/myReport.html");
+	  htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/myReport.html");
 
 	  htmlReporter.config().setDocumentTitle("Automation Report"); // Tile of report
 	  htmlReporter.config().setReportName("Functional Testing"); // Name of the report
@@ -58,28 +62,29 @@ public class homepagetest extends Basetest {
 	 }
   @Test(priority=1)
   public void myaccountlabelverification() {
-	  	  
+	  test = extent.createTest("myaccountlabelverification");
 	  Assert.assertTrue(homepage.verifymyaccountlabel(),"myaccount lable is not displayed");
   }
 	
   @Test(priority = 2)
   public void registerpagelabelverification() {
-	  
+	  test = extent.createTest("registerpagelabelverification");
 	Assert.assertTrue(homepage.verifyregisterbutton(), "register button is not displayed");  
 	  
   }
   
   @Test(priority=3)
    public void registerpagevalidation() {
-	 
+	  test = extent.createTest("registerpagevalidation");
 	  registerpage= homepage.clickonregister();
 	   Assert.assertEquals(homepage.validatingregisterpage(),"Register Account","register account is not displaying");
 	   
    }
   @Test(priority =4)
   public void loginrpagevalidation() {
+	  test = extent.createTest("loginrpagevalidation");
 	  homepage.clickonlogin();
-	  Assert.assertEquals(homepage.validatingloginpage(), "New Customer", "new cusotmer is not displaying");
+	  Assert.assertEquals(homepage.validatingloginpage(), "Ne Customer", "new cusotmer is not displaying");
 	  
   }
   
@@ -89,7 +94,21 @@ public class homepagetest extends Basetest {
 	 }
   
        @AfterMethod
-     public void teardown() {
+    
+    		 public void tearDown(ITestResult result) throws IOException {
+    			  if (result.getStatus() == ITestResult.FAILURE) {
+    				 
+    			  
+    			   test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+    			   test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
+    			   String screenshotPath = Testutil.getScreenshotas(driver, result.getName());
+    			   test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+    			  } else if (result.getStatus() == ITestResult.SKIP) {
+    			   test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+    			  }
+    			  else if (result.getStatus() == ITestResult.SUCCESS) {
+    			   test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+    			  }  
     	driver.quit(); 
      }
      
